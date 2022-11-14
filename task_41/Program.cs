a0c362c35a6565a699bcca9377cea4e43017da08
Griffon2012 <trifonov-ga@yandex.ru>
2022-11-14 22:59:52 +0300

    added task_41

---
+++ task_41/Program.cs
@@ -0,0 +1,32 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] EnteringNumbersByQuantity(int number)
+{
+    int[] arrayNumber = new int[number];
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write("Введите число: ");
+        arrayNumber[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return arrayNumber;
+}
+
+int GetCountNumbersAboveZero(int[] arrayNumber)
+{
+    int countNumbersAboveZero = 0;
+    for (int i = 0; i < arrayNumber.Length; i++)
+    {
+        if (arrayNumber[i] > 0)
+        {
+            countNumbersAboveZero++;
+        }
+    }
+
+    return countNumbersAboveZero;
+}
+
+Console.Write("Введите количество чисел: ");
+int countNumber = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumber = EnteringNumbersByQuantity(countNumber);
+Console.WriteLine($"Чисел больше нуля: {GetCountNumbersAboveZero(arrayNumber)}");
